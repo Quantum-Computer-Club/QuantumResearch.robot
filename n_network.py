0411a5da62bbfd085cbc19d7a14bdda0dcbce08e
ShuhaoWang <wsh@physics.so>
2016-01-21 15:26:58 +0800

    initial commit

---
+++ n_network.py
@@ -0,0 +1,45 @@
+from pybrain.datasets            import ClassificationDataSet
+from pybrain.utilities           import percentError
+from pybrain.tools.shortcuts     import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure.modules   import SoftmaxLayer
+from scipy import diag, arange, meshgrid, where
+from numpy.random import multivariate_normal
+import linecache
+import random
+
+samples = linecache.getlines('svm.txt')
+random.shuffle(samples)
+alldata = ClassificationDataSet(16, 1, nb_classes=2)
+for sample in samples:
+    sample_array = sample.split('\t')[0:16]
+    sample_result = sample.split('\t')[-1]
+    for element in range(0, len(sample_array)):
+        sample_array[element] = float(sample_array[element])
+    sample_result = int(sample_result)
+    alldata.addSample(sample_array, [sample_result])
+
+tstdata, trndata = alldata.splitWithProportion( 0.25 )
+print type(tstdata)
+trndata._convertToOneOfMany( )
+tstdata._convertToOneOfMany( )
+
+print "Number of training patterns: ", len(trndata)
+print "Input and output dimensions: ", trndata.indim, trndata.outdim
+print "First sample (input, target, class):"
+print trndata['input'][0], trndata['target'][0], trndata['class'][0]
+
+fnn = buildNetwork( trndata.indim, 5, trndata.outdim, outclass=SoftmaxLayer )
+trainer = BackpropTrainer( fnn, dataset=trndata, momentum=0.1, verbose=True, weightdecay=0.01)
+
+for i in range(20):
+    trainer.trainEpochs( 1 )
+    trnresult = percentError( trainer.testOnClassData(),
+                              trndata['class'] )
+    tstresult = percentError( trainer.testOnClassData(
+           dataset=tstdata ), tstdata['class'] )
+
+    print "epoch: %4d" % trainer.totalepochs, \
+          "  train error: %5.2f%%" % trnresult, \
+          "  test error: %5.2f%%" % tstresult
+
---
+++ svm.py
@@ -0,0 +1,47 @@
+from sklearn import svm
+import linecache
+import random
+import speaker
+
+ratio = 0.8
+
+samples = linecache.getlines('svm.txt')
+random.shuffle(samples)
+train = samples[0:int(len(samples)*ratio)]
+test = samples[int(len(samples)*ratio):len(samples)]
+X = []
+y = []
+X_test = []
+y_test = []
+
+for sample in train:
+    sample_array = sample.split('\t')[0:16]
+    sample_result = sample.split('\t')[-1]
+    for element in range(0, len(sample_array)):
+        sample_array[element] = float(sample_array[element])
+    sample_result = int(sample_result)
+    X.append(sample_array)
+    y.append(sample_result)
+
+for sample in test:
+    sample_array = sample.split('\t')[0:16]
+    sample_result = sample.split('\t')[-1]
+    for element in range(0, len(sample_array)):
+        sample_array[element] = float(sample_array[element])
+    sample_result = int(sample_result)
+    X_test.append(sample_array)
+    y_test.append(sample_result)
+
+clf = svm.SVC()
+clf.fit(X, y)
+
+truth = 0
+
+predicts = clf.predict(X_test)
+
+for i in range(0, len(predicts)):
+    if predicts[i] == y_test[i]:
+        truth += 1
+
+speaker.alert_trigger()
+print truth/(len(predicts)+0.0)
